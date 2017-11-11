b71927eec08d69d7206f836138c273512069d304
Fabio Pancot <fabio.pancot@gmail.com>
2017-11-11 20:08:26 +0100

    added qsort comparator function and create a sorting method to sort the reactances in order to compute median. next step will be compute the median.

--- src/impedance_calculator.c
+++ src/impedance_calculator.c
@@ -1,21 +1,47 @@
 #include <stdlib.h>
 #include <math.h>
+#include <types.h>
+
+#define COMPARE(a, b) (((a) > (b)) - ((a) < (b)))
 
 int calculate_mean_impedance(electrical_measurements_t *measurements,int used_elements){
 
-
   float impedance_sum = 0.0f;
   //have to add check_array_bounds to check if both arrays have same length
-  for(index = 0; index < used_elements; index++)
+  for(int index = 0; index < used_elements; index++)
   {
-    impedance_sum += math.sqrt(math.pow(measurements->resistances->array[index],2)+math.pow(measurements->resistances->array[index]));
+    impedance_sum += sqrt(pow(measurements->resistances.array[index],2)+pow(measurements->resistances.array[index],2));
   }
   measurements->mean_impedance = impedance_sum/used_elements;
 
 }
 
-void sort_electrical_measurements(electrical_measurements_t *measurements);
+int qsort_asc_comparator(const void* a, const void* b){
 
-float calculate_median_impedance(electrical_measurements_t *measurements);
+  float fa = *(const float*) a;
+float fb = *(const float*) b;
+return (fa > fb) - (fa < fb);
+}
+
+
+float* sort_electrical_measurements(electrical_measurements_t *measurements)
+{
+  int actual_size = measurements->reactances.used;
+  float* reactances_array = (float*)malloc(sizeof(float)*actual_size);
+  for (int index = 0;index < actual_size; index++)
+  {
+    reactances_array[index] = measurements->reactances.array[index];
+  }
+
+  qsort(reactances_array,actual_size,sizeof(float),qsort_asc_comparator);
+
+  return reactances_array;
+}
+
+
+float calculate_median_impedance(electrical_measurements_t *measurements){
+
+
+}
 
 int check_array_bounds(electrical_measurements_t *measurements);
